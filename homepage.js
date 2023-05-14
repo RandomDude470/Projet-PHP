caf03dedf80e43d26dc38948ef33d2b631fef52d
RandomDude470 <yassir.daoudi.2003@gmail.com>
2023-05-14 19:53:48 +0100

    corretion to the nav coloring

--- homepage.js
+++ homepage.js
@@ -10,13 +10,26 @@ nav_items.forEach(item=>{
                 element.style.height='0';
                 
             }
+            for (let index = 0; index < nav_items.length; index++) {
+                const element = nav_items[index];
+                element.style.backgroundColor='';
+                
+            }
+            item.style.backgroundColor= '35275F'
             tabs[0].style.height = '100vh'
+
         } else if(item==nav_items[1]) {
             for (let index = 0; index < tabs.length; index++) {
                 const element = tabs[index];
                 element.style.height='0';
                 
             }
+            for (let index = 0; index < nav_items.length; index++) {
+                const element = nav_items[index];
+                element.style.backgroundColor='';
+                
+            }
+            item.style.backgroundColor= '35275F'
             tabs[1].style.height = '100vh'
         }else{
             for (let index = 0; index < tabs.length; index++) {
@@ -24,6 +37,12 @@ nav_items.forEach(item=>{
                 element.style.height='0';
                 
             }
+            for (let index = 0; index < nav_items.length; index++) {
+                const element = nav_items[index];
+                element.style.backgroundColor='';
+                
+            }
+            item.style.backgroundColor= '35275F'
             tabs[2].style.height = '100vh'
         }
     }
